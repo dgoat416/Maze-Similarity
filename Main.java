72280102cd227ceabe9ad7d2079901ec86462c09
dgoat416 <55709445+dgoat416@users.noreply.github.com>
2020-11-03 15:10:52 -0800

    Created canGo method to assist with DFS that I need to add a body to
    - created a canGo method which will be useful in the dfs method
    - outlined dfs in my head and on paper
    - conceptually got the idea of what my algorithm must entail

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 // Deron Washington II
 import java.io.File;
 import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner6;
+
 import java.io.PrintWriter;
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.ArrayList;
+import java.awt.Point;
 
 /**
  * Class that houses the solution to CECS 328 Project 4 Similarity Mazes
@@ -70,7 +74,8 @@ public class Main
 		{
 			int preSum = n + s + w + e;
 
-			switch (preSum) {
+			switch (preSum) 
+			{
 				case 0:
 					return 4;
 				case 1:
@@ -96,11 +101,78 @@ public class Main
 		}
 	}
 
+
+	/**
+	 * Method to determine if we can perform DFS on a certain path
+	 * @param direction = the direction to test relative to the current position in the maze (static member)
+	 * 					  only acceptable values: (North, South, West, East)
+	 * @param maze = the current maze within mazes (static member)
+	 * @return = true if the proposed operation is allowed and false otherwise (if the proposed operation will lead us to
+	 * 			 array out of bounds exception or into a wall)
+	 */
+	public static boolean canGo(String direction, Room[][] maze, Point p)
+	{
+		Point testp = p;
+		Room room = maze[testp.x][testp.y];
+
+		if (direction.toUpperCase().equals("NORTH")) {
+			if (room.n == 1)
+				return false;
+			else
+				testp.x--;
+		} else if (direction.toUpperCase().equals("SOUTH")) {
+			if (room.s == 1)
+				return false;
+			else
+				testp.x++;
+		} else if (direction.toUpperCase().equals("WEST")) {
+			if (room.w == 1)
+				return false;
+			else
+				testp.y--;
+		} else if (direction.toUpperCase().equals("EAST")) {
+			if (room.e == 1)
+				return false;
+			else
+				testp.y++;
+		} else {
+			System.out.print("\n\nError in canGo(String, Room[][], Point) method. "
+					+ "No correct direction attribute specified.\n\n ");
+		}
+
+		// might be able to get rid of this because the maze defines it
+		// check if going the direction would produce an array out of bounds exception
+		try {
+			Room testRoom = maze[testp.x][testp.y];
+		} catch (ArrayIndexOutOfBoundsException AIOBE) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Method to perform a depth first search on the maze
+	 * @param maze = a 2d array holding Room objects 
+	 * @param start = starting index of the array (always 0, 0 in this project)
+	 * @return = a string representing the path that the dfs traveled by
+	 */
+	public static String depthFirstSearch(Room[][] maze, Point start)
+	{
+		String path = "";
+
+		// implement this using a stack for efficiency purposes
+
+
+		return path;
+	}
+
 	/**
 	 * Method to read the input from the file and populate the static reference of
 	 * maze accordingly
 	 */
-	public static void readInput(String fileName) {
+	public static void readInput(String fileName) 
+	 {
 		// Read input from file
 		File inFile = new File(fileName);
 		Scanner scan = null;
@@ -161,7 +233,8 @@ public class Main
 	 * @param mazeNum1   = least similar maze
 	 * @param mazeNum2   = corresponding least similar maze
 	 */
-	public static void writeOutput(String outputName, String mazeNum1, String mazeNum2) {
+	public static void writeOutput(String outputName, String mazeNum1, String mazeNum2) 
+	{
 		File outFile = new File(outputName);
 		java.io.PrintWriter writer = null;
 
@@ -179,10 +252,18 @@ public class Main
 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		
 		readInput("input.txt");
+
+		// // test canGoMethod
+		// System.out.println(canGo("west", mazes.get(0), new Point(0, 0))); // error
+		// System.out.println(canGo("south", mazes.get(0), new Point(0, 0))); // true
+		// System.out.println(canGo("east", mazes.get(0), new Point(0, 0))); // false
+		// System.out.println(canGo("north", mazes.get(0), new Point(0, 0))); // error
+
 		
 		System.out.print("Deron");
 
