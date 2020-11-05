be096502f872fe06e3aaf6cb91577d437c2a5aea
dgoat416 <55709445+dgoat416@users.noreply.github.com>
2020-11-05 12:04:23 -0800

    Finalized dfs algo (haven't completely implemented it)
    - Came up with a solid algorithm for depthFirstSearch method that I started to implement
    - Essentially use a stack to keep track of the "recursive calls" to the dfs. Due to the LIFO nature of the stack I will check in the opposite precedence when evaluating what the next path should be. So instead of checking in the original (NSWE) format I will check in EWSN precedence to ensure that the precedence is maintained through the stack (could have changed it to a Queue in theory but didn't want to.
    
    More notable points of the algo:
    - add to string path a capital letter for non-backtracking paths and a lowercase letter for backtracking
    
    - keep a hashmap to track the direction that the room was entered on for example if I enter a room using a north operation then I would store the value of the hashmap as the point say (2,0) and would store the value as 'N' for north
    
    - add to the path only when you pop off the stack or when doing backtracking step using hashmap
    
    - to backtrack use the hashmap to get the recorded value and perform the opposite operation to the current point within the maze

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 // Deron Washington II
 import java.io.File;
 import java.util.Scanner;
-
-import javax.lang.model.util.ElementScanner6;
-
 import java.io.PrintWriter;
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Stack;
 import java.util.ArrayList;
 import java.awt.Point;
 
@@ -61,21 +59,22 @@ public class Main
 		}
 
 		/**
-		 * Method to get the sum of the room The sum tells us how many options we have
-		 * to get out of the current room Since the maze has been defined as a '0' for
-		 * open spaces and a '1' for closed spaces I return the options instead of the
-		 * true "score" see below 0: We have 4 options to get out 1: We have 3 options
-		 * to get out 2: We have 2 options to get out 3: We have 1 option to get out 4:
-		 * We have no options to get out
+		 * Method to get the number of paths you can take out of the room (number of options)
+		 * Since the maze has been defined as a '0' for open spaces and a '1' for closed spaces
+		 * I return the options instead of the true room "score" 
+		 * see below 0: We have 4 options to get out 
+		 * 1: We have 3 options to get out 
+		 * 2: We have 2 options to get out 
+		 * 3: We have 1 option to get out 
+		 * 4: We have 0 options to get out
 		 * 
 		 * @return the number of options we have to get out of the current room
 		 */
-		public int getSum() 
+		public int getOptions() 
 		{
 			int preSum = n + s + w + e;
 
-			switch (preSum) 
-			{
+			switch (preSum) {
 				case 0:
 					return 4;
 				case 1:
@@ -92,6 +91,7 @@ public class Main
 			return Integer.MAX_VALUE;
 		}
 	
+
 		/**
 		 * Method to represent this object as a string
 		 */
@@ -102,51 +102,35 @@ public class Main
 	}
 
 
+
 	/**
 	 * Method to determine if we can perform DFS on a certain path
 	 * @param direction = the direction to test relative to the current position in the maze (static member)
 	 * 					  only acceptable values: (North, South, West, East)
-	 * @param maze = the current maze within mazes (static member)
+	 * @param room = current room we are in
 	 * @return = true if the proposed operation is allowed and false otherwise (if the proposed operation will lead us to
 	 * 			 array out of bounds exception or into a wall)
 	 */
-	public static boolean canGo(String direction, Room[][] maze, Point p)
+	public static boolean canGo(String direction, Room room)
 	{
-		Point testp = p;
-		Room room = maze[testp.x][testp.y];
 
 		if (direction.toUpperCase().equals("NORTH")) {
 			if (room.n == 1)
 				return false;
-			else
-				testp.x--;
 		} else if (direction.toUpperCase().equals("SOUTH")) {
 			if (room.s == 1)
 				return false;
-			else
-				testp.x++;
 		} else if (direction.toUpperCase().equals("WEST")) {
 			if (room.w == 1)
 				return false;
-			else
-				testp.y--;
 		} else if (direction.toUpperCase().equals("EAST")) {
 			if (room.e == 1)
 				return false;
-			else
-				testp.y++;
 		} else {
 			System.out.print("\n\nError in canGo(String, Room[][], Point) method. "
 					+ "No correct direction attribute specified.\n\n ");
 		}
 
-		// might be able to get rid of this because the maze defines it
-		// check if going the direction would produce an array out of bounds exception
-		try {
-			Room testRoom = maze[testp.x][testp.y];
-		} catch (ArrayIndexOutOfBoundsException AIOBE) {
-			return false;
-		}
 
 		return true;
 	}
@@ -154,14 +138,95 @@ public class Main
 	/**
 	 * Method to perform a depth first search on the maze
 	 * @param maze = a 2d array holding Room objects 
-	 * @param start = starting index of the array (always 0, 0 in this project)
 	 * @return = a string representing the path that the dfs traveled by
 	 */
-	public static String depthFirstSearch(Room[][] maze, Point start)
+	public static String depthFirstSearch(Room[][] maze)
 	{
+		/**
+		 * This is where I left off. We will use the tracker to determine 
+		 * where to go in the maze when backtracking
+		 * 
+		 * As of right now thinking of adding to the stack in the backwards
+		 * order EWSN due to the LIFO principle
+		 */
+
+		// this will track the path keeping all elements which we will return 
 		String path = "";
 
+		// this will track the path and remove elements to stay up to date when backtracking
+		List<Point> tracker = new ArrayList<Point>();
+
+		// keeps track of the point we are at in the maze
+		Point curr = new Point(0, 0);
+
+		// room we start at in the maze
+		Room start = maze[0][0];
+
+		// keeps track of the current room we are at in the maze
+		Room room = start;
+
 		// implement this using a stack for efficiency purposes
+		Stack<Room> callStack = new Stack<Room>();
+
+
+		// push starting point on the stack
+		callStack.push(start);
+		tracker.add(curr);
+		
+
+		// while not empty
+		while (!callStack.isEmpty())
+		{
+			// pop the node off the stack and process it as in a recursive function call
+			room = callStack.pop();
+
+			/* 
+				base case (no path out of the current room)
+				so we go back to the previous node			
+			*/
+			if(room.getOptions() == 0)
+			{
+				// (push it back on the stack ^^^)
+				// at starting position
+				if (room.equals(start))
+				{
+					// we are done
+					break;
+				}
+
+				
+
+			}
+
+			// go north
+			if(canGo("north", room))
+			{
+				// push northern node onto the stack 
+			}
+			
+			// go south
+			if (canGo("south", room))
+			{
+				// push southern node onto the stack
+			}
+
+
+			// go west
+			if (canGo("west", room))
+			{
+				// push western node onto the stack
+			}
+
+			// go east
+			if (canGo("east", room))
+			{
+				// push eastern node onto the stack
+			}
+
+
+
+
+		}
 
 
 		return path;
@@ -257,12 +322,14 @@ public class Main
 		// TODO Auto-generated method stub
 		
 		readInput("input.txt");
+		depthFirstSearch(mazes.get(0), new Point(0, 0));
 
 		// // test canGoMethod
 		// System.out.println(canGo("west", mazes.get(0), new Point(0, 0))); // error
 		// System.out.println(canGo("south", mazes.get(0), new Point(0, 0))); // true
 		// System.out.println(canGo("east", mazes.get(0), new Point(0, 0))); // false
 		// System.out.println(canGo("north", mazes.get(0), new Point(0, 0))); // error
+
 
 		
 		System.out.print("Deron");
